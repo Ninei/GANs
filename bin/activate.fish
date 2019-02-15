413e999f8a415160160252413041ef1e0e24e69d
Ninei <seeninei@gmail.com>
2019-02-15 15:03:41 +0900

    가상환경 재구성 시작

--- bin/activate.fish
+++ bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/workspace/Sogang/GANs"
+set -gx VIRTUAL_ENV "/Volumes/NineiDB/DB/Workspace/Sogang/GANs"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
